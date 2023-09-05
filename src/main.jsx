b317988e0cce5e84a250a96cbd3d55dd6e6c7b11
LuisMZampar <rm550531@fiap.com.br>
2023-09-05 17:27:43 -0300

    Criando a pasta router

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import EditarProdutos from './routes/index.jsx'
+import Error from './routes/Error/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/produtos",
+        element: <Produtos />
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProdutos />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
