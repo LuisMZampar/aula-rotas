5ff654b303fbf43aae5c9ad24e72b65c0d791fee
LuisMZampar <rm550531@fiap.com.br>
2023-09-15 22:21:03 -0300

    Arrumando erro do index.jsx

--- src/routes/EditarProdutos/Error/index.jsx
+++ src/routes/EditarProdutos/Error/index.jsx
@@ -1,4 +1,4 @@
-import { useParams, useNavigate } from "react-router-dom"
+import { useParams} from "react-router-dom"
 import {ListaProdutos } from "../../../components/Aparelhos"
  
 export default function EditarProdutos(){
