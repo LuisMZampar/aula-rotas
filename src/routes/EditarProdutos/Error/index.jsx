51cddfbdfe62b6c4ea770ec2f356291dc80a9c41
LuisMZampar <rm550531@fiap.com.br>
2023-09-15 22:20:07 -0300

    Atualizando o botão

--- src/routes/EditarProdutos/Error/index.jsx
+++ src/routes/EditarProdutos/Error/index.jsx
@@ -4,25 +4,16 @@ import {ListaProdutos } from "../../../components/Aparelhos"
 export default function EditarProdutos(){
 
     const lista = ListaProdutos
-    const navegacao = useNavigate()
     const {id} = useParams()
 
     const proc = lista.filter(prod => prod.id == id)
     const produto = proc[0]
-
-    const salvar = ()=>{
-        alert(`Produto: ${produto.nome} editado com sucesso!`)
-        return navegacao(`/produtos`)
-    }
-
-
 
     return(
         <main>
             <h1>{produto.nome}</h1>
             <p>Seu preço é de apenas: {produto.preco} </p>
             <img src= {produto.image} alt="Imagem Iphone 13" />
-            <button onClick={salvar}>Salvar</button>
         </main>
     )
 }
