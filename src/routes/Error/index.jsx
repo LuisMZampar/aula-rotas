3bf754fe91af05c0dd622da98056f901bcffa086
LuisMZampar <rm550531@fiap.com.br>
2023-09-15 21:19:17 -0300

    Inserindo o voltar para Tela inicial no Error

--- src/routes/Error/index.jsx
+++ src/routes/Error/index.jsx
@@ -1,7 +1,12 @@
-export default function Error(){
-    return(
+import { Link } from 'react-router-dom';
+
+export default function Error() {
+    return (
         <main>
+
             <h1>Erro 404 - Página não encontrada</h1>
+            <Link to="/">Voltar para a página inicial</Link>
+            
         </main>
     )
 }
